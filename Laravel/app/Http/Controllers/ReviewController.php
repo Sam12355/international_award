4bb8c85ac5cf64bac24af7a16fb8eff09bdaf400
Lakshan Rupasinghe <slaksh7@gmail.com>
2023-04-24 10:15:00 +0200

    feat(JRNL-5): reviewer queue and status workflow
    - ReviewController with index (queue), show, updateStatus
    - Review Blade views: queue listing and individual review form
    - Reviewer can set status to under_review/approved/rejected with notes
    - Author notified on status change via ArticleStatusChanged notification
    - Routes under /review prefix with role:reviewer,admin middleware

---
+++ Laravel/app/Http/Controllers/ReviewController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateArticleStatusRequest;
+use App\Models\Article;
+use App\Notifications\ArticleStatusChanged;
+use App\Services\ArticleService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ReviewController extends Controller
+{
+    public function __construct(
+        private readonly ArticleService $articleService,
+    ) {}
+
+    /**
+     * Reviewer dashboard — articles awaiting review.
+     */
+    public function index(Request $request): View
+    {
+        $articles = Article::with('journal', 'user')
+            ->whereIn('status', ['submitted', 'under_review'])
+            ->latest()
+            ->paginate(20);
+
+        return view('review.index', compact('articles'));
+    }
+
+    /**
+     * Show a single article for review with status-change form.
+     */
+    public function show(Article $article): View
+    {
+        $this->authorize('review', $article);
+
+        $article->load('journal', 'user', 'reviewAssignments.reviewer');
+
+        return view('review.show', compact('article'));
+    }
+
+    /**
+     * Update the article status (approve / reject / mark under review).
+     *
+     * Replaces the POST branch of legacy review_article.php.
+     */
+    public function updateStatus(UpdateArticleStatusRequest $request, Article $article): RedirectResponse
+    {
+        $this->authorize('review', $article);
+
+        $previousStatus = $article->status;
+
+        $this->articleService->updateStatus(
+            $article,
+            $request->validated('status'),
+            $request->validated('reviewer_notes'),
+        );
+
+        // Notify the author about the status change
+        $article->user->notify(new ArticleStatusChanged($article, $previousStatus));
+
+        return redirect()
+            ->route('review.show', $article)
+            ->with('success', 'Article status updated to: ' . $article->status);
+    }
+}
